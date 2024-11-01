162e3a6bf7881011a8d44852e467b593019d5d03
rrit5727 <rrit5727@gmail.com>
2024-11-01 15:57:14 +1100

    set-up server.js

--- server.js
+++ server.js
@@ -8,3 +8,19 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 
+ // Configure both serve-favicon & static middleware
+ // to serve from the production 'dist' folder
+//  app.use(favicon(path.join(__dirname, 'dist', 'favicon.ico')));
+ app.use(express.static(path.join(__dirname, 'dist')));
+
+ // The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+ const port = process.env.PORT || 3001;
+
+ app.listen(port, function(){
+  console.log(`Express app running on http://localhost: ${port}`)  
+ })
